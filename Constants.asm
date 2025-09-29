31b6794628943340d410022b3b2039b07a3f79c2
Mildanner <cianocriativo408@gmail.com>
2025-09-28 23:28:52 -0300

    Update Constants.asm

--- Constants.asm
+++ Constants.asm
@@ -193,6 +193,7 @@ bgm_Drowning:	equ ((ptr_mus92-MusicIndex)/4)+bgm__First
 bgm_Emerald:	equ ((ptr_mus93-MusicIndex)/4)+bgm__First
 bgm__Last:	equ ((ptr_musend-MusicIndex-4)/4)+bgm__First
 bgm_Options:	equ ((ptr_mus94-MusicIndex)/4)+bgm__First
+bgm_WaitingRoom:	equ ((ptr_mus95-MusicIndex)/4)+bgm__First
 
 ; Sound effects
 sfx__First:	equ $A0
