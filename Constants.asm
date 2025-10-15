b689c19a1586e661024851da30c050c68a6d2d25
Mildanner <cianocriativo408@gmail.com>
2025-10-15 18:23:22 -0300

    Order the BGM list

--- Constants.asm
+++ Constants.asm
@@ -191,9 +191,9 @@ bgm_Continue:	equ ((ptr_mus90-MusicIndex)/4)+bgm__First
 bgm_Credits:	equ ((ptr_mus91-MusicIndex)/4)+bgm__First
 bgm_Drowning:	equ ((ptr_mus92-MusicIndex)/4)+bgm__First
 bgm_Emerald:	equ ((ptr_mus93-MusicIndex)/4)+bgm__First
-bgm__Last:	equ ((ptr_musend-MusicIndex-4)/4)+bgm__First
 bgm_Options:	equ ((ptr_mus94-MusicIndex)/4)+bgm__First
 bgm_WaitingRoom:	equ ((ptr_mus95-MusicIndex)/4)+bgm__First
+bgm__Last:	equ ((ptr_musend-MusicIndex-4)/4)+bgm__First
 
 ; Sound effects
 sfx__First:	equ $A0
