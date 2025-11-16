11a307f394dad66c1d6a06e8a5017246514c7109
Mildanner <cianocriativo408@gmail.com>
2025-11-16 14:02:13 -0300

    Captions + Revert prototype drums

--- SampleTable.asm
+++ SampleTable.asm
@@ -2,17 +2,17 @@
 ; ---------------------------------------------------------------
 SampleTable:
 	;			type			pointer		Hz
-	dcSample	TYPE_DPCM, 		Kick, 		8000				; $81
-	dcSample	TYPE_PCM,		Snare,		16018				; $82
-	dcSample	TYPE_DPCM, 		Timpani, 	7250				; $83
-	dcSample	TYPE_PCM,		Clap,		17000				; $84
-	dcSample	TYPE_PCM,		Cymbal,		16953				; $85
-	dcSample	TYPE_NONE										; $86
-	dcSample	TYPE_NONE										; $87
-	dcSample	TYPE_DPCM, 		Timpani, 	9750				; $88
-	dcSample	TYPE_DPCM, 		Timpani, 	8750				; $89
-	dcSample	TYPE_DPCM, 		Timpani, 	7150				; $8A
-	dcSample	TYPE_DPCM, 		Timpani, 	7000				; $8B
+	dcSample	TYPE_DPCM, 		Kick, 		8000				; $81 - Kick
+	dcSample	TYPE_PCM,		Snare,		24000				; $82 - Snare
+	dcSample	TYPE_DPCM, 		Timpani, 	7250				; $83 - Timpani
+	dcSample	TYPE_PCM,		Clap,		17000				; $84 - Clap
+	dcSample	TYPE_PCM,		Cymbal,		16953				; $85 - Cymbal
+	dcSample	TYPE_NONE										; $86 - PLACEHOLDER
+	dcSample	TYPE_NONE										; $87 - PLACEHOLDER
+	dcSample	TYPE_DPCM, 		Timpani, 	9750				; $88 - Hi-Timpani
+	dcSample	TYPE_DPCM, 		Timpani, 	8750				; $89 - Mid-Timpani
+	dcSample	TYPE_DPCM, 		Timpani, 	7150				; $8A - Low-Timpani
+	dcSample	TYPE_DPCM, 		Timpani, 	7000				; $8B - Floor-Timpani
 	dcSample	TYPE_PCM,		SegaPCM,	0, FLAGS_SFX		; $8C	NOTE: sample rate is auto-detected from WAV file
 	dc.w	-1	; end marker
 
